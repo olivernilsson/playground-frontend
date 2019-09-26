6cc40794a04b4f3037d9f5f454ab0cd95f56d35b
Oliver <oliver.nilsson94@gmail.com>
2019-09-26 20:20:16 +0200

    added startpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import StartPage from "./views/StartPage"
+import StartPage from "./views/StartPage/StartPage"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <StartPage />
+    </div>
+  )
 }
 
 export default App
